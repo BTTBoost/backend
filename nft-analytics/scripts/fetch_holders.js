81ec65c12eb817d34f9458fd4359e67a40bec36f
Alex Tkachenko <skyalexx@gmail.com>
2022-04-27 19:53:13 +0200

    Save holders to db

--- nft-analytics/scripts/fetch_holders.js
+++ nft-analytics/scripts/fetch_holders.js
@@ -1,18 +1,51 @@
 import 'dotenv/config'
 import fetch from 'node-fetch'
+import pg from 'pg'
+import format from 'pg-format'
 
-// https://api.covalenthq.com/v1/1/tokens/0x3883f5e181fccaf8410fa61e12b59bad963fb645/token_holders/?quote-currency=USD&format=JSON&key=ckey_docs
 async function fetchHolders(network, token) {
+  const pageSize = 100000
   const url = `https://api.covalenthq.com/v1/${network}/tokens/${token}/token_holders/?` +
-    `quote-currency=USD&format=JSON&key=${process.env.COVALENT_API_KEY}&page-number=0&page-size=100000`
+    `quote-currency=USD&format=JSON&key=${process.env.COVALENT_API_KEY}&page-number=0&page-size=${pageSize}`
   return fetch(url)
     .then(response => response.json())
     .then(body => {
       if (body.error || !body.data) {
         throw new Error(body.error_message)
       }
+      if (body.data.items.length >= pageSize) {
+        throw new Error('too many holders')
+      }
       return body.data.items
     })
+}
+
+async function saveHolders(network, token, holders) {
+  const client = new pg.Client({
+    connectionString: process.env.DB_CONN_STRING,
+    ssl: { rejectUnauthorized: false },
+  })
+  await client.connect()
+
+  var count = 0
+  try {
+    await client.query('BEGIN')
+
+    await client.query(format('DELETE FROM token_holders_last WHERE network = %L AND token = %L', network, token))    
+    const query = 'INSERT INTO token_holders_last (network, token, holder, amount) VALUES %L'
+    
+    const values = holders.map(h => [network, token, h.address, h.balance])
+    const res = await client.query(format(query, values))
+    count = res.rowCount
+
+    await client.query('COMMIT')
+  } catch (e) {
+    console.error("Save error:", e)
+    await client.query('ROLLBACK')
+  }
+
+  await client.end()
+  return count
 }
 
 async function main() {
@@ -22,11 +55,13 @@ async function main() {
 
   return fetchHolders(network, token)
     .then(holders => {
-      console.log(`Fetched ${holders.length} holders!`)
+      console.log(`Fetched ${holders.length} holders`)
+      return holders
     })
-    .catch(error => {
-      console.error(`Failed to fetch holders: ${error}`)
-    })
+    .catch(error => console.error(`Failed to fetch holders: ${error}`))
+    .then(holders => saveHolders(network, token, holders))
+    .then(count => console.log(`Saved ${count} holders to db`))
+    .catch(error => console.error(`Failed to save holders: ${error}`))
 }
 
 main()
